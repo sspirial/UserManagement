b5955dc2865901a59b5fcfe083d894bac2ba9c0d
Munubi <sspirial@gmail.com>
2025-07-11 15:59:58 +0300

    milestone: phase 1 complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'accounts'
 urlpatterns = [
+    path('', views.index, name='index'),
     path('profile/', views.profile, name='profile'),
     path('signup/', views.signup, name='signup'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
